103fa914d0d972efd47a37718df402736bf94ab5
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2017-09-01 10:24:53 -0500

    Delete fibonacci.py

--- fibonacci.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(x):
-    ''' This function generates the first x values of the Fibonacci sequence as a list.'''
-    if (x < 3):
-        fibseq = [1] * x
-    else:
-        fibseq = [1] * 2
-        for i in range(2, x):
-            fibseq.append(fibseq[i - 2] + fibseq[i - 1])
-    return fibseq
